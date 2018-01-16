cb74c0b2f2e51603ae57e389e521a1cffde284c9
Yufei Ren <yufei.ren@thomsonreuters.com>
2018-01-16 15:48:01 +0800

    minor change

--- kubernetes/kops_create.sh
+++ kubernetes/kops_create.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-kops create cluster $NAME \
+export S3_BUCKET=kiwi-k8s-state-store
+export KOPS_STATE_STORE=s3://${S3_BUCKET}
+export AWS_AVAILABILITY_ZONES=us-east-1a,us-east-1b
+export NAME=kiwi.k8s.local
+
+echo "create S3 bucket $S3_BUCKET"
+aws s3api create-bucket \
+--bucket $S3_BUCKET \
+--region us-east-1
+
+echo "create k8s cluster"
+kops create cluster --name $NAME \
 --cloud aws \
 --state $KOPS_STATE_STORE \
 --zones=$AWS_AVAILABILITY_ZONES \
